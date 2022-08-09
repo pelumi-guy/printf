55dfc4633eed8c8544beca97df4ee766702f41ab
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-08 18:23:11 -0700

    Mandatory tasks

--- 0-handlers.c
+++ 0-handlers.c
@@ -1,37 +1,65 @@
 #include "main.h"
 
 /**
- * print_char - prints a character in the format string
- * Description:
- * Return: 1
- */
-
-int print_char(va_list args)
-{
-	_putchar(va_arg(args, int));
-	return (1);
-}
-
-/**
  * print_string - prints a character in the format string
  * Description:
- * Return: 1
+ * @args: array of arguments
+ * Return: length of string
  */
 
 int print_string(va_list args)
 {
-	_putchar(va_arg(args, int));
-	return (1);
+	char *str = (va_arg(args, char *));
+	int i, len = 0;
+
+	for (i = 0; str[i]; i++, len++)
+		_putchar(str[i]);
+
+	return (len);
 }
 
 /**
- * print_char - prints a character in the format string
+ * recursivePrintNum - prints an integer number recursively
  * Description:
- * Return: 1
+ * @num: Integer to be printed
+ * Return: number of characters printed
+ */
+int recursivePrintNum(int num)
+{
+	int len = 1;
+
+	if (num / 10)
+		len = (1 + (recursivePrintNum(num / 10)));
+
+	_putchar((num % 10) + '0');
+
+	return (len);
+}
+
+/**
+ * print_number - prints an integer in the format string
+ * Description:
+ * @args: Array of variadic arguments
+ * Return: number of characters printed
  */
 
-int print_modulus(va_list args)
+int print_number(va_list args)
 {
-	_putchar(va_arg(args, int));
-	return (1);
+	int argNum = (va_arg(args, int));
+	int num, len = 0;
+
+	if (argNum < 0)
+	{
+		num = -argNum;
+		_putchar('-');
+		len++;
+	}
+	else
+	{
+		num = argNum;
+	}
+
+	len += recursivePrintNum(num);
+
+	return (len);
 }
